71e73d657e0bb29675602e9ec515a4f10a546183
beanstown106 <nbenis106@gmail.com>
2015-11-09 20:58:44 -0500

    angler: facelock properties
    *update for N
    
    Change-Id: I1be3c998b083da85a4109eca13d4f455c9e6f37a

--- device.mk
+++ device.mk
@@ -455,6 +455,13 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
+# facelock properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=2500 \
+    ro.facelock.rec_timeout=3500 \
+    ro.facelock.est_max_time=600
+
 # miracast props
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1﻿
