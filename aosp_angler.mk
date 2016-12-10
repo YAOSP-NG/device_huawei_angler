994ce3417e8fa425f51a56e01216f55a74eb8b4e
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-12-10 10:41:43 +0100

    Add fingerprint for NMF26F
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -43,3 +43,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     AOSPLinks
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
