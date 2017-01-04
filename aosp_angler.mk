13be05fb9b21569519feffed85ce20ba71b20f78
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-01-04 21:58:49 +0100

    Add fingerprint for N4F26J
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -43,3 +43,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     AOSPLinks
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
